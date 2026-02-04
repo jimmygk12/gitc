4121f9698714ae39189b75e7173ca36039ef8dbb
unknown <gkkunjam25@gmail.com>
2026-02-04 16:42:47 +0530

    java

--- hlw.cpp
+++ hlw.cpp
@@ -1,112 +1,14 @@
 #include <iostream>
-#include <fstream>
-#include <string>
 using namespace std;
-
-class Book {
-public:
-    int id;
-    string title;
-    string author;
-    bool issued;
-};
-
-void addBook() {
-    Book b;
-    ofstream file("books.txt", ios::app);
-
-    cout << "Enter Book ID: ";
-    cin >> b.id;
-    cin.ignore();
-
-    cout << "Enter Title: ";
-    getline(cin, b.title);
-
-    cout << "Enter Author: ";
-    getline(cin, b.author);
-
-    b.issued = false;
-
-    file << b.id << "|" << b.title << "|" << b.author << "|" << b.issued << endl;
-    file.close();
-
-    cout << "Book added successfully!\n";
-}
-
-void displayBooks() {
-    Book b;
-    ifstream file("books.txt");
-
-    cout << "\n--- Book List ---\n";
-    while (file >> b.id) {
-        file.ignore();
-        getline(file, b.title, '|');
-        getline(file, b.author, '|');
-        file >> b.issued;
-        file.ignore();
-
-        cout << b.id << " | " << b.title << " | " << b.author
-             << " | " << (b.issued ? "Issued" : "Available") << endl;
-    }
-    file.close();
-}
-
-void issueBook() {
-    int searchId;
-    Book b;
-    bool found = false;
-
-    ifstream file("books.txt");
-    ofstream temp("temp.txt");
-
-    cout << "Enter Book ID to issue: ";
-    cin >> searchId;
-
-    while (file >> b.id) {
-        file.ignore();
-        getline(file, b.title, '|');
-        getline(file, b.author, '|');
-        file >> b.issued;
-        file.ignore();
-
-        if (b.id == searchId && !b.issued) {
-            b.issued = true;
-            found = true;
-        }
-        temp << b.id << "|" << b.title << "|" << b.author << "|" << b.issued << endl;
-    }
-
-    file.close();
-    temp.close();
-
-    remove("books.txt");
-    rename("temp.txt", "books.txt");
-
-    if (found)
-        cout << "Book issued successfully!\n";
-    else
-        cout << "Book not available!\n";
-}
-
 int main() {
-    int choice;
-    do {
-        cout << "\n--- Library Management System ---\n";
-        cout << "1. Add Book\n";
-        cout << "2. View Books\n";
-        cout << "3. Issue Book\n";
-        cout << "4. Exit\n";
-        cout << "Enter choice: ";
-        cin >> choice;
-
-        switch (choice) {
-        case 1: addBook(); break;
-        case 2: displayBooks(); break;
-        case 3: issueBook(); break;
-        case 4: cout << "Exiting...\n"; break;
-        default: cout << "Invalid choice!\n";
+    int n=3;
+    int num = 1;
+    for (i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            cout<<num<<"";
+            num++;
         }
-    } while (choice != 4);
-
+        cout<<endl;
+    }
     return 0;
 }

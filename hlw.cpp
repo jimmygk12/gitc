8c6c081ed3f2f4f623ff49a7f19985a57a7a67ad
unknown <gkkunjam25@gmail.com>
2026-02-04 18:42:26 +0530

    new file

--- hlw.cpp
+++ hlw.cpp
@@ -3,12 +3,12 @@ using namespace std;
 int main() {
     int n=3;
     int num = 1;
-    for (i=0;i<n;i++){
-        for(j=0;j<n;j++){
+    for (int i = 0; i < n; i++){
+        for(int j=0;j<n;j++){
             cout<<num<<"";
             num++;
         }
         cout<<endl;
     }
     return 0;
-}
+}   
